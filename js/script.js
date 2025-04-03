39646cfe344bd61a08e3157007057e95558b8c6f
SergiyLuchko <LuchkoSergiy85@gmail.com>
2025-04-03 18:59:32 +0300

    Add  backdrop to movies fn

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const global = { curerntPage: window.location.pathname };
 
-
 // Display 20 most popular movies
 async function displayPopularMovies() {
   const { results } = await fetchAPIData('movie/popular');
@@ -91,27 +90,30 @@ alt="${show.name}"
 }
 
 // Display movie details
-async function displayMoviesDetails () {
-  const movieId = window.location.search.split('=')[1]
- const movie = await fetchAPIData(`movie/${movieId}`)
- const div = document.createElement('div')
- div.innerHTML =`       <div class="details-top">
+async function displayMoviesDetails() {
+  const movieId = window.location.search.split('=')[1];
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  // Overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
+
+  const div = document.createElement('div');
+  div.innerHTML = `       <div class="details-top">
  <div>
  ${
-  movie.poster_path
-    ? `
+   movie.poster_path
+     ? `
   <img
   src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
   class="card-img-top"
   alt="${movie.title}"
 />`
-    : `
+     : `
 <img
 src="images/no-image.jpg"
 class="card-img-top"
 alt="${movie.title}"
 />`
-}
+ }
  </div>
  <div>
    <h2>${movie.title}</h2>
@@ -125,24 +127,34 @@ alt="${movie.title}"
    </p>
    <h5>Genres</h5>
    <ul class="list-group">
-   ${movie.genres.map((genre)=> `<li>${genre.name}</li>`).join('')}
+   ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
    </ul>
-   <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+   <a href="${
+     movie.homepage
+   }" target="_blank" class="btn">Visit Movie Homepage</a>
  </div>
 </div>
 <div class="details-bottom">
  <h2>Movie Info</h2>
  <ul>
-   <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
-   <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
-   <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+   <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+     movie.budget
+   )}</li>
+   <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+     movie.revenue
+   )}</li>
+   <li><span class="text-secondary">Runtime:</span> ${
+     movie.runtime
+   } minutes</li>
    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
  </ul>
  <h4>Production Companies</h4>
- <div class="list-group">${movie.production_companies.map((company)=>`<span>${company.name}</span>`).join(', ')}</div>
+ <div class="list-group">${movie.production_companies
+   .map((company) => `<span>${company.name}</span>`)
+   .join(', ')}</div>
 </div>
- `
- document.querySelector('#movie-details').appendChild(div)
+ `;
+  document.querySelector('#movie-details').appendChild(div);
 }
 
 // Show spinner
@@ -164,7 +176,29 @@ function highlightActiveLink() {
 }
 
 function addCommasToNumber(number) {
-return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',')
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+// Display backdrop on detail-pages
+function displayBackgroundImage(type, backgroundPath){
+  const overlayDiv  =  document.createElement('div')
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`
+  overlayDiv.style.backgroundSize = 'cover'
+  overlayDiv.style.backgroundPosition = 'center'
+  overlayDiv.style.backgroundRepeat = 'no-repeat'
+  overlayDiv.style.height = '100vh'
+  overlayDiv.style.width = '100vw'
+  overlayDiv.style.position = 'absolute'
+  // overlayDiv.style.top = '0'
+  // overlayDiv.style.left = '0'
+  overlayDiv.style.inset = '0'
+  overlayDiv.style.zIndex = '-1'
+  overlayDiv.style.opacity = '0.1'
+
+  if(type === 'movie'){
+    document.querySelector('#movie-details').appendChild(overlayDiv)
+  }else {
+    document.querySelector('#show-details').appendChild(overlayDiv)
+  }
 }
 
 // Init app
@@ -176,11 +210,11 @@ function init() {
       console.log('home');
       break;
     case '/shows.html':
-      displayPopularShows()
+      displayPopularShows();
       console.log('shows');
       break;
     case '/movie-details.html':
-      displayMoviesDetails()
+      displayMoviesDetails();
       console.log('movie-details');
       break;
     case '/tv-details.html':
